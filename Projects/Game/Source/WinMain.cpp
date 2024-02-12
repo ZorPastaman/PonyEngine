44dbc84ddad99de388920a77191c963eccbed0f6
Vladimir Popov <zor1994@gmail.com>
2024-02-12 17:56:18 +0200

    WinMain

---
+++ Projects/Game/Source/WinMain.cpp
@@ -0,0 +1,17 @@
+/***************************************************
+ * MIT License                                     *
+ *                                                 *
+ * Copyright (c) 2023-present Vladimir Popov       *
+ *                                                 *
+ * Email: zor1994@gmail.com                        *
+ * Repo: https://github.com/ZorPastaman/PonyEngine *
+ ***************************************************/
+
+#include <windows.h>
+
+import Game;
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
+{
+	return Game::GameMain();
+}
