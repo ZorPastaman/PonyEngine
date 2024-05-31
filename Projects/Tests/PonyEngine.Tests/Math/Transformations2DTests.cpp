1b98f114f894e61f87365bb364203721d402ab10
Vladimir Popov <zor1994@gmail.com>
2024-05-31 12:45:49 +0300

    More Transformations2D tests

--- Projects/Tests/PonyEngine.Tests/Math/Transformations2DTests.cpp
+++ Projects/Tests/PonyEngine.Tests/Math/Transformations2DTests.cpp
@@ -115,24 +115,72 @@ namespace Math
 
 		TEST_METHOD(RsMatrixFromAngleScalingTest)
 		{
-			constexpr float angle = 1.065f;
-			constexpr auto scaling = PonyEngine::Math::Vector2<float>(-2.f, 3.f);
+			float angle = 1.065f;
+			auto scaling = PonyEngine::Math::Vector2<float>(2.f, 3.f);
 			auto matrix = PonyEngine::Math::RsMatrix(angle, scaling);
-			Assert::AreEqual(-0.97, static_cast<double>(matrix.M00()), 0.001);
-			Assert::AreEqual(-1.75, static_cast<double>(matrix.M10()), 0.001);
+			Assert::AreEqual(0.97, static_cast<double>(matrix.M00()), 0.001);
+			Assert::AreEqual(1.75, static_cast<double>(matrix.M10()), 0.001);
 			Assert::AreEqual(-2.625, static_cast<double>(matrix.M01()), 0.001);
 			Assert::AreEqual(1.454, static_cast<double>(matrix.M11()), 0.001);
+
+			angle = 1.065f;
+			scaling = PonyEngine::Math::Vector2<float>(-4.f, 6.f);
+			matrix = PonyEngine::Math::RsMatrix(angle, scaling);
+			Assert::AreEqual(-1.938, static_cast<double>(matrix.M00()), 0.001);
+			Assert::AreEqual(-3.5, static_cast<double>(matrix.M10()), 0.001);
+			Assert::AreEqual(-5.249, static_cast<double>(matrix.M01()), 0.001);
+			Assert::AreEqual(2.907, static_cast<double>(matrix.M11()), 0.001);
+
+			angle = 1.065f;
+			scaling = PonyEngine::Math::Vector2<float>(3.f, -2.f);
+			matrix = PonyEngine::Math::RsMatrix(angle, scaling);
+			Assert::AreEqual(1.454, static_cast<double>(matrix.M00()), 0.001);
+			Assert::AreEqual(2.625, static_cast<double>(matrix.M10()), 0.001);
+			Assert::AreEqual(1.75, static_cast<double>(matrix.M01()), 0.001);
+			Assert::AreEqual(-0.97, static_cast<double>(matrix.M11()), 0.001);
+
+			angle = 1.065f;
+			scaling = PonyEngine::Math::Vector2<float>(-0.5f, -1.f);
+			matrix = PonyEngine::Math::RsMatrix(angle, scaling);
+			Assert::AreEqual(-0.243, static_cast<double>(matrix.M00()), 0.001);
+			Assert::AreEqual(-0.438, static_cast<double>(matrix.M10()), 0.001);
+			Assert::AreEqual(0.875, static_cast<double>(matrix.M01()), 0.001);
+			Assert::AreEqual(-0.485, static_cast<double>(matrix.M11()), 0.001);
 		}
 
 		TEST_METHOD(RsMatrixFromRotationMatrixScalingTest)
 		{
-			constexpr auto rotation = PonyEngine::Math::Matrix2x2<float>(0.485f, 0.875f, -0.875f, 0.485f);
-			constexpr auto scaling = PonyEngine::Math::Vector2<float>(-2.f, 3.f);
+			auto rotation = PonyEngine::Math::Matrix2x2<float>(0.485f, 0.875f, -0.875f, 0.485f);
+			auto scaling = PonyEngine::Math::Vector2<float>(2.f, 3.f);
 			auto matrix = PonyEngine::Math::RsMatrix(rotation, scaling);
-			Assert::AreEqual(-0.97, static_cast<double>(matrix.M00()), 0.001);
-			Assert::AreEqual(-1.75, static_cast<double>(matrix.M10()), 0.001);
+			Assert::AreEqual(0.97, static_cast<double>(matrix.M00()), 0.001);
+			Assert::AreEqual(1.75, static_cast<double>(matrix.M10()), 0.001);
 			Assert::AreEqual(-2.625, static_cast<double>(matrix.M01()), 0.001);
 			Assert::AreEqual(1.455, static_cast<double>(matrix.M11()), 0.001);
+
+			rotation = PonyEngine::Math::Matrix2x2<float>(0.485f, 0.875f, -0.875f, 0.485f);
+			scaling = PonyEngine::Math::Vector2<float>(-4.f, 6.f);
+			matrix = PonyEngine::Math::RsMatrix(rotation, scaling);
+			Assert::AreEqual(-1.94, static_cast<double>(matrix.M00()), 0.001);
+			Assert::AreEqual(-3.5, static_cast<double>(matrix.M10()), 0.001);
+			Assert::AreEqual(-5.25, static_cast<double>(matrix.M01()), 0.001);
+			Assert::AreEqual(2.91, static_cast<double>(matrix.M11()), 0.001);
+
+			rotation = PonyEngine::Math::Matrix2x2<float>(0.485f, 0.875f, -0.875f, 0.485f);
+			scaling = PonyEngine::Math::Vector2<float>(3.f, -2.f);
+			matrix = PonyEngine::Math::RsMatrix(rotation, scaling);
+			Assert::AreEqual(1.455, static_cast<double>(matrix.M00()), 0.001);
+			Assert::AreEqual(2.625, static_cast<double>(matrix.M10()), 0.001);
+			Assert::AreEqual(1.75, static_cast<double>(matrix.M01()), 0.001);
+			Assert::AreEqual(-0.97, static_cast<double>(matrix.M11()), 0.001);
+
+			rotation = PonyEngine::Math::Matrix2x2<float>(0.485f, 0.875f, -0.875f, 0.485f);
+			scaling = PonyEngine::Math::Vector2<float>(-0.5f, -1.f);
+			matrix = PonyEngine::Math::RsMatrix(rotation, scaling);
+			Assert::AreEqual(-0.243, static_cast<double>(matrix.M00()), 0.001);
+			Assert::AreEqual(-0.438, static_cast<double>(matrix.M10()), 0.001);
+			Assert::AreEqual(0.875, static_cast<double>(matrix.M01()), 0.001);
+			Assert::AreEqual(-0.485, static_cast<double>(matrix.M11()), 0.001);
 		}
 
 		TEST_METHOD(TrsMatrixFromTranslationAngleScalingTest)
@@ -156,7 +204,7 @@ namespace Math
 		{
 			constexpr auto rotation = PonyEngine::Math::Matrix2x2<float>(0.485f, 0.875f, -0.875f, 0.485f);
 			constexpr auto scaling = PonyEngine::Math::Vector2<float>(-2.f, 3.f);
-			const auto rsMatrix = PonyEngine::Math::RsMatrix(rotation, scaling);
+			constexpr auto rsMatrix = PonyEngine::Math::RsMatrix(rotation, scaling);
 			auto matrix = PonyEngine::Math::TrsMatrix(rsMatrix);
 			Assert::AreEqual(-0.97, static_cast<double>(matrix.M00()), 0.001);
 			Assert::AreEqual(-1.75, static_cast<double>(matrix.M10()), 0.001);
@@ -174,7 +222,7 @@ namespace Math
 			constexpr auto translation = PonyEngine::Math::Vector2<float>(-3, 7);
 			constexpr auto rotation = PonyEngine::Math::Matrix2x2<float>(0.485f, 0.875f, -0.875f, 0.485f);
 			constexpr auto scaling = PonyEngine::Math::Vector2<float>(-2.f, 3.f);
-			const auto rsMatrix = PonyEngine::Math::RsMatrix(rotation, scaling);
+			constexpr auto rsMatrix = PonyEngine::Math::RsMatrix(rotation, scaling);
 			auto matrix = PonyEngine::Math::TrsMatrix(translation, rsMatrix);
 			Assert::AreEqual(-0.97, static_cast<double>(matrix.M00()), 0.001);
 			Assert::AreEqual(-1.75, static_cast<double>(matrix.M10()), 0.001);
@@ -197,8 +245,20 @@ namespace Math
 
 		TEST_METHOD(AngleFromRsMatrixTest)
 		{
-			constexpr auto rsMatrix = PonyEngine::Math::Matrix2x2<float>(-0.97f, -1.75f, -2.625f, 1.455f);
-			const float angle = PonyEngine::Math::ExtractAngleFromRsMatrix(rsMatrix);
+			auto rsMatrix = PonyEngine::Math::Matrix2x2<float>(0.97f, 1.75f, -2.625f, 1.455f);
+			float angle = PonyEngine::Math::ExtractAngleFromRsMatrix(rsMatrix);
+			Assert::AreEqual(1.065, static_cast<double>(angle), 0.001);
+
+			rsMatrix = PonyEngine::Math::Matrix2x2<float>(-1.94f, -3.5f, -5.25f, 2.91f);
+			angle = PonyEngine::Math::ExtractAngleFromRsMatrix(rsMatrix);
+			Assert::AreEqual(-2.077, static_cast<double>(angle), 0.001);
+
+			rsMatrix = PonyEngine::Math::Matrix2x2<float>(1.455f, 2.625f, 1.75f, -0.97f);
+			angle = PonyEngine::Math::ExtractAngleFromRsMatrix(rsMatrix);
+			Assert::AreEqual(1.065, static_cast<double>(angle), 0.001);
+
+			rsMatrix = PonyEngine::Math::Matrix2x2<float>(-0.243f, -0.438f, 0.875f, -0.485f);
+			angle = PonyEngine::Math::ExtractAngleFromRsMatrix(rsMatrix);
 			Assert::AreEqual(-2.077, static_cast<double>(angle), 0.001);
 		}
 
@@ -211,8 +271,29 @@ namespace Math
 
 		TEST_METHOD(RotationMatrixFromRsMatrixTest)
 		{
-			constexpr auto rsMatrix = PonyEngine::Math::Matrix2x2<float>(-0.97f, -1.75f, -2.625f, 1.455f);
+			auto rsMatrix = PonyEngine::Math::Matrix2x2<float>(0.97f, 1.75f, -2.625f, 1.455f);
 			PonyEngine::Math::Matrix2x2<float> matrix = PonyEngine::Math::ExtractRotationMatrixFromRsMatrix(rsMatrix);
+			Assert::AreEqual(0.485, static_cast<double>(matrix.M00()), 0.001);
+			Assert::AreEqual(0.875, static_cast<double>(matrix.M10()), 0.001);
+			Assert::AreEqual(-0.875, static_cast<double>(matrix.M01()), 0.001);
+			Assert::AreEqual(0.485, static_cast<double>(matrix.M11()), 0.001);
+
+			rsMatrix = PonyEngine::Math::Matrix2x2<float>(-1.94f, -3.5f, -5.25f, 2.91f);
+			matrix = PonyEngine::Math::ExtractRotationMatrixFromRsMatrix(rsMatrix);
+			Assert::AreEqual(-0.485, static_cast<double>(matrix.M00()), 0.001);
+			Assert::AreEqual(-0.875, static_cast<double>(matrix.M10()), 0.001);
+			Assert::AreEqual(0.875, static_cast<double>(matrix.M01()), 0.001);
+			Assert::AreEqual(-0.485, static_cast<double>(matrix.M11()), 0.001);
+
+			rsMatrix = PonyEngine::Math::Matrix2x2<float>(1.455f, 2.625f, 1.75f, -0.97f);
+			matrix = PonyEngine::Math::ExtractRotationMatrixFromRsMatrix(rsMatrix);
+			Assert::AreEqual(0.485, static_cast<double>(matrix.M00()), 0.001);
+			Assert::AreEqual(0.875, static_cast<double>(matrix.M10()), 0.001);
+			Assert::AreEqual(-0.875, static_cast<double>(matrix.M01()), 0.001);
+			Assert::AreEqual(0.485, static_cast<double>(matrix.M11()), 0.001);
+
+			rsMatrix = PonyEngine::Math::Matrix2x2<float>(-0.243f, -0.438f, 0.875f, -0.485f);
+			matrix = PonyEngine::Math::ExtractRotationMatrixFromRsMatrix(rsMatrix);
 			Assert::AreEqual(-0.485, static_cast<double>(matrix.M00()), 0.001);
 			Assert::AreEqual(-0.875, static_cast<double>(matrix.M10()), 0.001);
 			Assert::AreEqual(0.875, static_cast<double>(matrix.M01()), 0.001);
@@ -231,10 +312,25 @@ namespace Math
 
 		TEST_METHOD(ScalingFromRsMatrixTest)
 		{
-			constexpr auto rsMatrix = PonyEngine::Math::Matrix2x2<float>(-0.97f, -1.75f, -2.625f, 1.455f);
+			auto rsMatrix = PonyEngine::Math::Matrix2x2<float>(0.97f, 1.75f, -2.625f, 1.455f);
 			PonyEngine::Math::Vector2<float> scaling = PonyEngine::Math::ExtractScalingFromRsMatrix(rsMatrix);
 			Assert::AreEqual(2., static_cast<double>(scaling.X()), 0.005);
-			Assert::AreEqual(-3., static_cast<double>(scaling.Y()), 0.005);
+			Assert::AreEqual(3., static_cast<double>(scaling.Y()), 0.005);
+
+			rsMatrix = PonyEngine::Math::Matrix2x2<float>(-1.94f, -3.5f, -5.25f, 2.91f);
+			scaling = PonyEngine::Math::ExtractScalingFromRsMatrix(rsMatrix);
+			Assert::AreEqual(4., static_cast<double>(scaling.X()), 0.005);
+			Assert::AreEqual(-6., static_cast<double>(scaling.Y()), 0.005);
+
+			rsMatrix = PonyEngine::Math::Matrix2x2<float>(1.455f, 2.625f, 1.75f, -0.97f);
+			scaling = PonyEngine::Math::ExtractScalingFromRsMatrix(rsMatrix);
+			Assert::AreEqual(3., static_cast<double>(scaling.X()), 0.005);
+			Assert::AreEqual(-2., static_cast<double>(scaling.Y()), 0.005);
+
+			rsMatrix = PonyEngine::Math::Matrix2x2<float>(-0.243f, -0.438f, 0.875f, -0.485f);
+			scaling = PonyEngine::Math::ExtractScalingFromRsMatrix(rsMatrix);
+			Assert::AreEqual(0.5, static_cast<double>(scaling.X()), 0.005);
+			Assert::AreEqual(1., static_cast<double>(scaling.Y()), 0.005);
 		}
 
 		TEST_METHOD(ScalingFromTrsMatrixTest)
@@ -258,28 +354,73 @@ namespace Math
 		TEST_METHOD(RotateVectorTest)
 		{
 			constexpr auto vector = PonyEngine::Math::Vector2<float>(-2.f, 3.f);
-			float angle = 1.56f;
+			float angle = 1.01f;
 			PonyEngine::Math::Vector2<float> rotated = PonyEngine::Math::Rotate(vector, angle);
-			Assert::AreEqual(-3.021, static_cast<double>(rotated.X()), 0.001);
-			Assert::AreEqual(-1.968, static_cast<double>(rotated.Y()), 0.001);
+			Assert::AreEqual(-3.604, static_cast<double>(rotated.X()), 0.001);
+			Assert::AreEqual(-0.098, static_cast<double>(rotated.Y()), 0.001);
+
+			angle = 2.52f;
+			rotated = PonyEngine::Math::Rotate(vector, angle);
+			Assert::AreEqual(-0.121, static_cast<double>(rotated.X()), 0.001);
+			Assert::AreEqual(-3.604, static_cast<double>(rotated.Y()), 0.001);
+
+			angle = -2.49f;
+			rotated = PonyEngine::Math::Rotate(vector, angle);
+			Assert::AreEqual(3.41, static_cast<double>(rotated.X()), 0.001);
+			Assert::AreEqual(-1.173, static_cast<double>(rotated.Y()), 0.001);
+
+			angle = -0.78f;
+			rotated = PonyEngine::Math::Rotate(vector, angle);
+			Assert::AreEqual(0.688, static_cast<double>(rotated.X()), 0.001);
+			Assert::AreEqual(3.539, static_cast<double>(rotated.Y()), 0.001);
+
+			angle = 0.f;
+			rotated = PonyEngine::Math::Rotate(vector, angle);
+			Assert::AreEqual(-2., static_cast<double>(rotated.X()), 0.001);
+			Assert::AreEqual(3., static_cast<double>(rotated.Y()), 0.001);
+
+			angle = std::numbers::pi_v<float> / 2.f;
+			rotated = PonyEngine::Math::Rotate(vector, angle);
+			Assert::AreEqual(-3., static_cast<double>(rotated.X()), 0.001);
+			Assert::AreEqual(-2., static_cast<double>(rotated.Y()), 0.001);
+
+			angle = -std::numbers::pi_v<float> / 2.f;
+			rotated = PonyEngine::Math::Rotate(vector, angle);
+			Assert::AreEqual(3., static_cast<double>(rotated.X()), 0.001);
+			Assert::AreEqual(2., static_cast<double>(rotated.Y()), 0.001);
+
+			angle = std::numbers::pi_v<float>;
+			rotated = PonyEngine::Math::Rotate(vector, angle);
+			Assert::AreEqual(2., static_cast<double>(rotated.X()), 0.001);
+			Assert::AreEqual(-3., static_cast<double>(rotated.Y()), 0.001);
 		}
 
 		TEST_METHOD(TranformPointTest)
 		{
-			constexpr auto trsMatrix = PonyEngine::Math::Matrix3x3<float>(-0.97f, -1.75f, 0.f, -2.625f, 1.455f, 0.f, -3.f, 7.f, 1.f);
 			constexpr auto vector = PonyEngine::Math::Vector2<float>(4.f, -1.f);
-			const PonyEngine::Math::Vector2<float> transformed = PonyEngine::Math::TransformPoint(trsMatrix, vector);
-			Assert::AreEqual(-4.252, static_cast<double>(transformed.X()), 0.005);
-			Assert::AreEqual(-1.452, static_cast<double>(transformed.Y()), 0.005);
+			auto trsMatrix = PonyEngine::Math::Matrix3x3<float>(0.97f, 1.75f, 0.f, -2.625f, 1.455f, 0.f, -3.f, 7.f, 1.f);
+			PonyEngine::Math::Vector2<float> transformed = PonyEngine::Math::TransformPoint(trsMatrix, vector);
+			Assert::AreEqual(3.505, static_cast<double>(transformed.X()), 0.005);
+			Assert::AreEqual(12.545, static_cast<double>(transformed.Y()), 0.005);
+
+			trsMatrix = PonyEngine::Math::Matrix3x3<float>(2.298f, -1.929f, 0.f, -0.450f, -0.536f, 0.f, 5.f, -1.f, 1.f);
+			transformed = PonyEngine::Math::TransformPoint(trsMatrix, vector);
+			Assert::AreEqual(14.643, static_cast<double>(transformed.X()), 0.005);
+			Assert::AreEqual(-8.176, static_cast<double>(transformed.Y()), 0.005);
 		}
 
 		TEST_METHOD(TranformDirectionTest)
 		{
-			constexpr auto trsMatrix = PonyEngine::Math::Matrix3x3<float>(-0.97f, -1.75f, 0.f, -2.625f, 1.455f, 0.f, -3.f, 7.f, 1.f);
 			constexpr auto vector = PonyEngine::Math::Vector2<float>(4.f, -1.f);
-			const PonyEngine::Math::Vector2<float> transformed = PonyEngine::Math::TransformDirection(trsMatrix, vector);
-			Assert::AreEqual(-1.252, static_cast<double>(transformed.X()), 0.005);
-			Assert::AreEqual(-8.452, static_cast<double>(transformed.Y()), 0.005);
+			auto trsMatrix = PonyEngine::Math::Matrix3x3<float>(0.97f, 1.75f, 0.f, -2.625f, 1.455f, 0.f, -3.f, 7.f, 1.f);
+			PonyEngine::Math::Vector2<float> transformed = PonyEngine::Math::TransformDirection(trsMatrix, vector);
+			Assert::AreEqual(6.505, static_cast<double>(transformed.X()), 0.005);
+			Assert::AreEqual(5.545, static_cast<double>(transformed.Y()), 0.005);
+
+			trsMatrix = PonyEngine::Math::Matrix3x3<float>(2.298f, -1.929f, 0.f, -0.450f, -0.536f, 0.f, 5.f, -1.f, 1.f);
+			transformed = PonyEngine::Math::TransformDirection(trsMatrix, vector);
+			Assert::AreEqual(9.643, static_cast<double>(transformed.X()), 0.005);
+			Assert::AreEqual(-7.176, static_cast<double>(transformed.Y()), 0.005);
 		}
 
 		TEST_METHOD(ConstexprCompilationTest)
