05082f4891f9722b8c0729d97b8c4dd3fdf17437
Vladimir Popov <zor1994@gmail.com>
2024-10-11 19:18:53 +0300

    GUID docs and test

---
+++ Projects/Tests/PonyBase.Tests/Utility/GuidUtilityTests.cpp
@@ -0,0 +1,30 @@
+/***************************************************
+ * MIT License                                     *
+ *                                                 *
+ * Copyright (c) 2023-present Vladimir Popov       *
+ *                                                 *
+ * Email: zor1994@gmail.com                        *
+ * Repo: https://github.com/ZorPastaman/PonyEngine *
+ ***************************************************/
+
+#include "CppUnitTest.h"
+
+#include "PonyBase/Core/COM/Framework.h"
+
+import PonyBase.GuidUtility;
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Utility
+{
+	TEST_CLASS(GuidUtilityTests)
+	{
+		TEST_METHOD(ToStringTest)
+		{
+			auto guid = GUID{.Data1 = 0xD2DF2879, .Data2 = 0xB16A, .Data3 = 0xBABA};
+			*reinterpret_cast<std::uint64_t*>(guid.Data4) = 0x6517BB1B6D4D;
+
+			Assert::AreEqual("D2DF2879-B16A-BABA-6517BB1B6D4D", PonyBase::Utility::ToString(guid).c_str());
+		}
+	};
+}
