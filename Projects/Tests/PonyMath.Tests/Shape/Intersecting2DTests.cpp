bea06c7d9cd71acaa2a9913f591f9180110da558
Vladimir Popov <zor1994@gmail.com>
2025-03-08 19:44:35 +0200

    LineTests

--- Projects/Tests/PonyMath.Tests/Shape/Intersecting2DTests.cpp
+++ Projects/Tests/PonyMath.Tests/Shape/Intersecting2DTests.cpp
@@ -420,5 +420,114 @@ namespace Shape
 			Assert::IsFalse(PonyMath::Shape::AreIntersecting(ray, obr, 1.f));
 			Assert::IsFalse(PonyMath::Shape::AreIntersecting(ray, obr, 100.f));
 		}
+
+		TEST_METHOD(LineLineTest)
+		{
+			const auto line0Normal = PonyMath::Core::Vector2<float>(0.8f, 0.7f).Normalized();
+			const auto line0 = PonyMath::Shape::Line<float>(line0Normal, 1.f);
+			auto line1Normal = PonyMath::Core::Vector2<float>(0.5f, 0.7f).Normalized();
+			auto line1 = PonyMath::Shape::Line<float>(line1Normal, 2.f);
+			Assert::IsTrue(PonyMath::Shape::AreIntersecting(line0, line1));
+			Assert::IsTrue(PonyMath::Shape::AreIntersecting(line1, line0));
+
+			line1 = PonyMath::Shape::Line<float>(line0Normal, 2.f);
+			Assert::IsFalse(PonyMath::Shape::AreIntersecting(line0, line1));
+			Assert::IsFalse(PonyMath::Shape::AreIntersecting(line1, line0));
+
+			line1 = PonyMath::Shape::Line<float>(-line0Normal, 2.f);
+			Assert::IsFalse(PonyMath::Shape::AreIntersecting(line0, line1));
+			Assert::IsFalse(PonyMath::Shape::AreIntersecting(line1, line0));
+
+			line1Normal = PonyMath::Core::Vector2<float>(-line0Normal.Y(), line0Normal.X());
+			line1 = PonyMath::Shape::Line<float>(line1Normal, 2.f);
+			Assert::IsTrue(PonyMath::Shape::AreIntersecting(line0, line1));
+			Assert::IsTrue(PonyMath::Shape::AreIntersecting(line1, line0));
+		}
+
+		TEST_METHOD(LineRayTest)
+		{
+			auto lineNormal = PonyMath::Core::Vector2<float>(0.5f, 0.5f).Normalized();
+			auto linePoint = PonyMath::Core::Vector2<float>(5.f, 2.f);
+			auto line = PonyMath::Shape::Line<float>(lineNormal, linePoint);
+			constexpr auto rayOrigin = PonyMath::Core::Vector2<float>(-2.f, 1.f);
+			auto rayDirection = PonyMath::Core::Vector2<float>(0.8f, 0.7f).Normalized();
+			const auto ray = PonyMath::Shape::Ray2D<float>(rayOrigin, rayDirection);
+			Assert::IsTrue(PonyMath::Shape::AreIntersecting(line, ray));
+			Assert::IsFalse(PonyMath::Shape::AreIntersecting(line, ray, 1.f));
+			Assert::IsTrue(PonyMath::Shape::AreIntersecting(line, ray, 100.f));
+
+			line = PonyMath::Shape::Line<float>(lineNormal, rayOrigin);
+			Assert::IsFalse(PonyMath::Shape::AreIntersecting(line, ray));
+			Assert::IsFalse(PonyMath::Shape::AreIntersecting(line, ray, 1.f));
+			Assert::IsFalse(PonyMath::Shape::AreIntersecting(line, ray, 100.f));
+
+			lineNormal = PonyMath::Core::Vector2<float>(-rayDirection.Y(), rayDirection.X());
+			linePoint = rayOrigin + rayDirection * 0.00001f;
+			line = PonyMath::Shape::Line<float>(lineNormal, linePoint);
+			Assert::IsFalse(PonyMath::Shape::AreIntersecting(line, ray));
+			Assert::IsFalse(PonyMath::Shape::AreIntersecting(line, ray, 1.f));
+			Assert::IsFalse(PonyMath::Shape::AreIntersecting(line, ray, 100.f));
+
+			linePoint = rayOrigin - rayDirection * 2.f;
+			line = PonyMath::Shape::Line<float>(lineNormal, linePoint);
+			Assert::IsFalse(PonyMath::Shape::AreIntersecting(line, ray));
+			Assert::IsFalse(PonyMath::Shape::AreIntersecting(line, ray, 1.f));
+			Assert::IsFalse(PonyMath::Shape::AreIntersecting(line, ray, 100.f));
+		}
+
+		TEST_METHOD(LineAabrTest)
+		{
+			auto lineNormal = PonyMath::Core::Vector2<float>(0.5f, 0.5f).Normalized();
+			auto linePoint = PonyMath::Core::Vector2<float>(5.f, 2.f);
+			auto line = PonyMath::Shape::Line<float>(lineNormal, linePoint);
+			constexpr auto aabrCenter = PonyMath::Core::Vector2<float>(3.f, 4.f);
+			constexpr auto aabrExtents = PonyMath::Core::Vector2<float>(4.f, 5.f);
+			const auto aabr = PonyMath::Shape::AABR<float>(aabrCenter, aabrExtents);
+			Assert::IsTrue(PonyMath::Shape::AreIntersecting(line, aabr));
+
+			lineNormal = -lineNormal;
+			line = PonyMath::Shape::Line<float>(lineNormal, linePoint);
+			Assert::IsTrue(PonyMath::Shape::AreIntersecting(line, aabr));
+
+			lineNormal = PonyMath::Core::Vector2<float>(-0.1f, 0.5f).Normalized();
+			line = PonyMath::Shape::Line<float>(lineNormal, linePoint);
+			Assert::IsTrue(PonyMath::Shape::AreIntersecting(line, aabr));
+
+			linePoint = PonyMath::Core::Vector2<float>(-7.f, 12.f);
+			line = PonyMath::Shape::Line<float>(lineNormal, linePoint);
+			Assert::IsFalse(PonyMath::Shape::AreIntersecting(line, aabr));
+
+			linePoint = PonyMath::Core::Vector2<float>(9.f, 10.f);
+			line = PonyMath::Shape::Line<float>(lineNormal, linePoint);
+			Assert::IsTrue(PonyMath::Shape::AreIntersecting(line, aabr));
+		}
+
+		TEST_METHOD(LineObrTest)
+		{
+			auto lineNormal = PonyMath::Core::Vector2<float>(0.5f, 0.5f).Normalized();
+			auto linePoint = PonyMath::Core::Vector2<float>(-5.f, 2.f);
+			auto line = PonyMath::Shape::Line<float>(lineNormal, linePoint);
+			constexpr auto aabrCenter = PonyMath::Core::Vector2<float>(3.f, 4.f);
+			constexpr auto aabrExtents = PonyMath::Core::Vector2<float>(4.f, 5.f);
+			const auto aabr = PonyMath::Shape::AABR<float>(aabrCenter, aabrExtents);
+			const auto obr = PonyMath::Shape::OBR<float>(aabr, 105.f * PonyMath::Core::DegToRad<float>);
+			Assert::IsTrue(PonyMath::Shape::AreIntersecting(line, obr));
+
+			lineNormal = -lineNormal;
+			line = PonyMath::Shape::Line<float>(lineNormal, linePoint);
+			Assert::IsTrue(PonyMath::Shape::AreIntersecting(line, obr));
+
+			lineNormal = PonyMath::Core::Vector2<float>(-0.1f, 0.5f).Normalized();
+			line = PonyMath::Shape::Line<float>(lineNormal, linePoint);
+			Assert::IsTrue(PonyMath::Shape::AreIntersecting(line, obr));
+
+			linePoint = PonyMath::Core::Vector2<float>(-7.f, 12.f);
+			line = PonyMath::Shape::Line<float>(lineNormal, linePoint);
+			Assert::IsFalse(PonyMath::Shape::AreIntersecting(line, obr));
+
+			linePoint = PonyMath::Core::Vector2<float>(2.f, 5.f);
+			line = PonyMath::Shape::Line<float>(lineNormal, linePoint);
+			Assert::IsTrue(PonyMath::Shape::AreIntersecting(line, obr));
+		}
 	};
 }
