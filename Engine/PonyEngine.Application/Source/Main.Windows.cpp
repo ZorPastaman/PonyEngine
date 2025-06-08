df67598353ec6f0e2efa6ae86d879efad33f7dda
Vladimir Popov <zor1994@gmail.com>
2025-06-08 15:01:34 +0300

    Main with correct exception management

--- Engine/PonyEngine.Application/Source/Main.Windows.cpp
+++ Engine/PonyEngine.Application/Source/Main.Windows.cpp
@@ -19,6 +19,8 @@ import PonyEngine.Utility;
 
 int APIENTRY WinMain(const HINSTANCE, const HINSTANCE, const PSTR, const int)
 {
+	int exitCode = PonyEngine::Application::ExitCodes::Success;
+
 	try
 	{
 		PonyEngine::Application::Windows::SetProcessPriority(ABOVE_NORMAL_PRIORITY_CLASS);
@@ -26,8 +28,26 @@ int APIENTRY WinMain(const HINSTANCE, const HINSTANCE, const PSTR, const int)
 		PonyEngine::Application::Windows::CreateConsole(CP_UTF8);
 #endif
 
-		auto app = std::make_unique<PonyEngine::Application::App>();
-		app.reset();
+		try
+		{
+			PONY_CONSOLE(PonyEngine::Log::LogType::Info, "Creates application.");
+			auto app = std::make_unique<PonyEngine::Application::App>();
+			PONY_CONSOLE(PonyEngine::Log::LogType::Info, "Application destroyed.");
+		}
+		catch (const std::exception& e)
+		{
+			PONY_CONSOLE_E(e, "On application.");
+			MessageBoxA(nullptr, PonyEngine::Utility::SafeFormat("Exception on application: '{}'.", e.what()).c_str(), "PonyEngine exception", MB_OK | MB_ICONERROR);
+
+			exitCode = PonyEngine::Application::ExitCodes::ApplicationException;
+		}
+		catch (...)
+		{
+			PONY_CONSOLE(PonyEngine::Log::LogType::Exception, "Unknown exception - on application.");
+			MessageBoxA(nullptr, "Exception on application.", "PonyEngine exception", MB_OK | MB_ICONERROR);
+
+			exitCode = PonyEngine::Application::ExitCodes::ApplicationException;
+		}
 
 #if PONY_CREATE_CONSOLE
 		PonyEngine::Application::Windows::DestroyConsole();
@@ -38,15 +58,15 @@ int APIENTRY WinMain(const HINSTANCE, const HINSTANCE, const PSTR, const int)
 		PONY_CONSOLE_E(e, "On main.");
 		MessageBoxA(nullptr, PonyEngine::Utility::SafeFormat("Exception on main: '{}'.", e.what()).c_str(), "PonyEngine exception", MB_OK | MB_ICONERROR);
 
-		return PonyEngine::Application::ExitCodes::MainException;
+		exitCode = PonyEngine::Application::ExitCodes::MainException;
 	}
 	catch (...)
 	{
 		PONY_CONSOLE(PonyEngine::Log::LogType::Exception, "Unknown exception - on main.");
 		MessageBoxA(nullptr, "Unknown exception on main.", "PonyEngine exception", MB_OK | MB_ICONERROR);
 
-		return PonyEngine::Application::ExitCodes::MainException;
+		exitCode = PonyEngine::Application::ExitCodes::MainException;
 	}
 
-	return PonyEngine::Application::ExitCodes::Success;
+	return exitCode;
 }
